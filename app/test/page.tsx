84fdd8aebbcd0a3a7dc3256c8b167634aaa0cf7c
Calvin Ducharme <calvin@azrael.local>
2024-05-02 22:07:15 -0700

    hey

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
 
 export default function IndexPage() {
-    const [x, setX] = useState(0)
-    const [y, setY] = useState(0)
-    const [z, setZ] = useState(0)
+    const x = useRef(0)
+    const y = useRef(0)
+    const z = useRef(0)
+    const x2 = useRef(0)
+    const y2 = useRef(0)
+    const z2 = useRef(0)
+    const sliderRef = useRef(null)
     useEffect(() => {
         async function requestPermission() {
             document.getElementById('debug-blue')!.innerText = "hello"
@@ -29,22 +33,16 @@ export default function IndexPage() {
 
         function addDeviceMotionListener() {
             window.addEventListener('devicemotion', (event: DeviceMotionEvent) => {
-                const red: any = document.getElementById('debug-red');
-                const green: any = document.getElementById('debug-green');
-                const blue: any = document.getElementById('debug-blue');
-                if (!red || !green || !blue) return;
-
+                x2.current = event?.acceleration?.x || 0
+                y2.current = event?.acceleration?.y || 0
+                z2.current = event?.acceleration?.z || 0
             });
         }
         function addDeviceOrientationListener() {
             window.addEventListener('deviceorientation', (event: DeviceOrientationEvent) => {
-                const red: any = document.getElementById('debug-red-2');
-                const green: any = document.getElementById('debug-green-2');
-                const blue: any = document.getElementById('debug-blue-2');
-                if (!red || !green || !blue) return;
-                setX(event?.alpha || 0); red.innerText = event?.alpha?.toString();
-                setY(event?.beta || 0); green.innerText = event?.beta?.toString();
-                setZ(event?.gamma || 0); blue.innerText = event?.gamma?.toString();
+                x.current = event?.alpha || 0
+                y.current = event?.beta || 0
+                z.current = event?.gamma || 0
             });
         }
 
@@ -61,9 +59,10 @@ export default function IndexPage() {
         function draw() {
             ctx.clearRect(0, 0, width, height)
             ctx.beginPath()
-            ctx.arc(centerX + (x), centerY + (y), 30, 0, 2 * Math.PI)
+            ctx.arc(centerX + (x.current), centerY + (y.current), 30, 0, 2 * Math.PI)
             ctx.fillStyle = 'rgba(255, 165, 0, 0.5)'
             ctx.fill()
+            console.log("g")
             requestAnimationFrame(draw)
         }
 
@@ -74,13 +73,22 @@ export default function IndexPage() {
     return (
         <div className="relative bg-black overflow-hidden" style={{ height: '100svh' }}>
             <div className="absolute top-0 left-0 h-full w-full z-20 bg-blue-900">
-                <p id="debug-red" className="text-red-300">-</p>
-                <p id="debug-green" className="text-green-300">-</p>
-                <p id="debug-blue" className="text-blue-300">-</p>
-                <p id="debug-red-2" className="text-red-300">-</p>
-                <p id="debug-green-2" className="text-green-300">-</p>
-                <p id="debug-blue-2" className="text-blue-300">-</p>
+                <p id="debug-red" className="text-red-300">{x.current}</p>
+                <p id="debug-green" className="text-green-300">{y.current}</p>
+                <p id="debug-blue" className="text-blue-300">{z.current}</p>
+                <p id="debug-red-2" className="text-red-300">{x2.current}</p>
+                <p id="debug-green-2" className="text-green-300">{y2.current}</p>
+                <p id="debug-blue-2" className="text-blue-300">{z2.current}</p>
                 <button id="permission-button" className="text-white">allow</button>
+                <input
+                    type="range"
+                    min="-100"
+                    max="100"
+                    value={x.current}
+                    ref={sliderRef}
+                    onChange={(e) => x.current = parseInt(e.target.value)}
+                    className="slider"
+                />
             </div>
             <canvas id="canvas" style={{ pointerEvents: 'none' }} className="absolute top-0 left-0 z-30 h-full w-full"></canvas>
         </div>
